b6bfc2ef86a87acf4f6bb65bc3970ad46c026650
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-03-06 17:42:10 +0000

    queuing system

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import { createClient } from "redis";
+const client = createClient();
+client
+  .on("connect", (val) => {
+    console.log("Redis client connected to the server");
+  })
+  .on("error", (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
